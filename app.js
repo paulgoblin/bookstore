23c3175dcf6796fa03e1da17044413a39a6cfb77
Paul Richter <mpr75@cornell.edu>
2015-12-11 09:08:14 -0800

    heroku ready (maybe)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 
 var mongoose = require('mongoose');
-mongoose.connect(MONGO_URL, function(err){
+mongoose.connect(process.env.MONGOLAB_URI || MONGO_URL, function(err){
   if(err) return console.log(err);
   console.log(`MongoDB connected to ${MONGO_URL}`);
 });
